c9d6cc7c19e3c04890e9b0e7d743055348944ca4
metalmaninfr <alex_blandford@hotmail.com>
2018-02-12 16:40:01 +0100

    login inté fini

---
+++ login.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="assets/css/reset.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <title>RANDOM IMG</title>
+</head>
+<body>
+  <div class="loginPage-container">
+    <div class="login-container">
+      <div class="login-logo">
+
+      </div>
+        <div class="login-title">
+          <h2>Welcome to Mynd</h2>
+        </div>
+        <div class="login-email">
+          <input type="email" placeholder="E-mail">
+        </div>
+        <div class="login-password">
+          <input type="password" placeholder="Password">
+        </div>
+        <div class="login-btn">
+          <a href="#">Sign in</a>
+        </div>
+        <div class="login-other">
+          <p>Or</p>
+        </div>
+        <div class="login-facebook">
+          <div class="login-facebook-img">
+          <img src="assets/img/facebook.png" alt="">
+        </div>
+          <a href="#">Continue with facebook</a>
+        </div>
+        <div class="login-google">
+          <div class="login-google-img">
+            <img src="assets/img/Google.png" alt="">
+          </div>
+          <a href="#">Continue with google</a>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
