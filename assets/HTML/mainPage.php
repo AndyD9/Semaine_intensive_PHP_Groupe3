809ac8ee3ea69acafbd76503347eb7bfcca668f2
metalmaninfr <alex_blandford@hotmail.com>
2018-02-13 10:49:07 +0100

    mainPage.php added

---
+++ assets/HTML/mainPage.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Main Page</title>
+</head>
+<body>
+  <header>
+    <div class="header-logoCcontainer">
+      <img src="#" alt="">
+      <div></div>
+    </div>
+    <div class="header-searchBar">
+      <input type="search" placeholder="Search photos">
+    </div>
+    <div class="header-profilePicture">
+      <img src="#" alt="">
+      <div></div>
+    </div>
+  </header>
+  <div class="main-container">
+    <span class="firstLine"></span>
+    <h2>Trending</h2>
+    <h2>New</h2>
+    <span></span>
+  </div>
+    <div class="img-container">
+      <div class="img-150-300">
+        <img src="../img/alex-radelich-365048.jpg" alt="">
+      </div>
+  </div>
+
+</body>
+</html>
